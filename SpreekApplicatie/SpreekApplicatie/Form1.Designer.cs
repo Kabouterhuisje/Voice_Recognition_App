71036d93a3227bc0dfc98ccc9987a0cf0cb69fe5
Dennis Tijbosch <d.tijbosch@yahoo.com>
2015-10-28 16:50:54 +0100

    Project to GitHub
    Just adding

---
+++ SpreekApplicatie/SpreekApplicatie/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SpreekApplicatie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnInport = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.rtxtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(432, 12);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(113, 23);
+            this.btnRead.TabIndex = 6;
+            this.btnRead.Text = "Read Text";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnInport
+            // 
+            this.btnInport.Location = new System.Drawing.Point(433, 42);
+            this.btnInport.Name = "btnInport";
+            this.btnInport.Size = new System.Drawing.Size(112, 23);
+            this.btnInport.TabIndex = 7;
+            this.btnInport.Text = "Start Inport";
+            this.btnInport.UseVisualStyleBackColor = true;
+            this.btnInport.Click += new System.EventHandler(this.btnInport_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(432, 72);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(113, 23);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop Inport";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // rtxtInput
+            // 
+            this.rtxtInput.Location = new System.Drawing.Point(13, 13);
+            this.rtxtInput.Multiline = true;
+            this.rtxtInput.Name = "rtxtInput";
+            this.rtxtInput.Size = new System.Drawing.Size(413, 350);
+            this.rtxtInput.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 375);
+            this.Controls.Add(this.rtxtInput);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnInport);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnInport;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox rtxtInput;
+    }
+}
+
---
+++ SpreekApplicatie/SpreekApplicatie/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech; 
+using System.Speech.Recognition; 
+using System.Speech.Synthesis; 
+using System.Threading;
+using System.Diagnostics;
+
+namespace SpreekApplicatie
+{
+    public partial class Form1 : Form
+    {
+        
+        SpeechRecognitionEngine MyEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer MySyn = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // Message if there is no text in the text box
+            if (string.IsNullOrEmpty(rtxtInput.Text))
+            {
+                MySyn.SpeakAsync("Sorry,  I can't read without text, Enter text first");
+            }
+            else
+            {
+                MySyn.Dispose(); 
+                MySyn = new SpeechSynthesizer(); 
+                MySyn.SpeakAsync(rtxtInput.Text); // print the text in text box
+            }
+        }
+
+        private void btnInport_Click(object sender, EventArgs e)
+        {
+            btnInport.Enabled = false;
+            btnStop.Enabled = true;
+            // Creating list with strings the computer can print
+            Choices sList = new Choices();
+            sList.Add(new string[] { "say hello", "how are you", "exit", "what is the current time", "thank you" });
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(sList);
+            Grammar gr = new Grammar(gb);
+
+            // Try to print the text in the text box
+            try
+            {
+                MyEngine.RequestRecognizerUpdate();
+                MyEngine.LoadGrammar(gr);
+                MyEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(MyEngine_SpeechRecognized);
+                MyEngine.SetInputToDefaultAudioDevice();
+                MyEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void MyEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text != "")
+            {
+                if (e.Result.Text == "exit")
+                {
+                    Application.Exit();
+                }
+                else if (e.Result.Text == "how are you")
+                {
+                    MySyn.SpeakAsync("I'm fine, thanks for asking");
+                }
+                else if (e.Result.Text == "what is the current time")
+                {
+                    MySyn.SpeakAsync("The current time is " + DateTime.Now.ToLongTimeString());
+                }
+                else if (e.Result.Text == "thank you")
+                {
+                    MySyn.SpeakAsync("no thanks, have a nice day");
+                }
+                else if (e.Result.Text == "say hello")
+                {
+                    MySyn.SpeakAsync("hello, nice to see you again");
+                }
+                rtxtInput.Text = rtxtInput.Text + " " + e.Result.Text.ToString() + Environment.NewLine;
+            }
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            MyEngine.RecognizeAsyncStop();
+            btnStop.Enabled = false;
+            btnInport.Enabled = true;
+        }
+    }
+}
